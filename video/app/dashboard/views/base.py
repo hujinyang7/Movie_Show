70bf4334dd8b1f581921390e432eb89a5859d451
Your jaye <73574091@qq.com>
2020-03-18 03:13:38 +0800

    3.18 by jaye

---
+++ video/app/dashboard/views/base.py
@@ -0,0 +1,13 @@
+#coding:utf-8
+
+from django.views.generic import View
+from app.libs.base_render import render_to_response
+
+
+class Index(View):
+    TEMPLATE = '/templates/dashboard/index.html'
+
+    def get(self,request):
+        return render_to_response(request,self.TEMPLATE,)
+
+
